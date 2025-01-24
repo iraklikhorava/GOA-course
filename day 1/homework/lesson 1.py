b72585654f53bccd4f5e7e279169c085c24afd68
iraklikhorava <iraklihkhorava2007@gmail.com>
2025-01-24 19:26:43 +0400

    first commit

---
+++ day 1/homework/lesson 1.py
@@ -0,0 +1,8 @@
+name = "irakli"
+surname = "khorava"
+height = 178
+age = 17
+str_loves_gaming = "loves gaming"
+does_not_love_gaming = False
+#want to make big sentence using all of these
+print("My" + " " + "name" + " " + "is" + " " + name + " " + surname + ", " + "is" + " " + str(age) + " " + "years old" + ", " + "height" + " " + str(height) + ", " + str_loves_gaming + ".") 
