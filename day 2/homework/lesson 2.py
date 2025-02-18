4d64ff40c51f55daf1657f3876599b9a10aa2317
iraklikhorava <iraklihkhorava2007@gmail.com>
2025-02-18 21:40:15 +0400

    second commit

---
+++ day 2/homework/lesson 2.py
@@ -0,0 +1,48 @@
+#გააკეთეთ ორ რიცხვს შორის თითოეულ მათემატიკურ ოპერაციაზე 5-5 მაგალითი (+,-,*,/) და დაპრინტეთ.
+print(5+5)
+print(11+17)
+print(14+3)
+print(50+20)
+print(19+91)
+
+print(100-83)
+print(10-7)
+print(13-12)
+print(67-42)
+print(25-21)
+
+print(4*8)
+print(5*10)
+print(13*3)
+print(27*5)
+print(45*21)
+
+print(196/14)
+print(25/5)
+print(23/4)
+print(17/10)
+print(1924/13)
+#ცვლადები სადაც იქნება თქვენი სახელი, თქვენი გვარი, თქვენი ასაკი და დაბეჭდეთ ერთ დიდ წინადადებაში.
+name = "Irakli"
+surname = "Khorava"
+age = 17
+print(f"My name is {name} {surname}, I am {age} years old.")
+
+#შექმენი 9 ცვლადი, 3 ცვლადი იყოს Str, 3 ცვლადი int, 3 ცვლადი float, და შემდეგ გამოიტანე ტერმინალში 9-ვე ცვლადი print-ის მეშვეობით.
+#str
+country = "Georgia"
+city = "Poti"
+name = "Irakli"
+
+#int
+age = 17
+year = 2025
+height = 177
+
+#float
+weight = 71.2
+temperature = 36.7
+eyesight = -3.0
+
+#combine all of these info
+print(country, city, name, age, year, height, weight, temperature, eyesight)
