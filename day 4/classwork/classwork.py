67d864efaabf6f8a8f5c432cdb3bddd5965ce354
iraklikhorava <iraklihkhorava2007@gmail.com>
2025-03-07 23:37:00 +0400

    fourth commit

---
+++ day 4/classwork/classwork.py
@@ -0,0 +1,41 @@
+# ორი ცვლადის შექმნა
+a = 6
+b = 14
+# მესამე ცვლადში მათი ჯამის შენახვა
+c = a + b
+print(c)
+
+#5 არასწორი კოდი
+# print(name)
+# name="Irakli"     თანმიმდევრობა არ არის დაცული
+name="Irakli"
+print(name)
+
+# a=4
+# b=2
+# print("a" + b)     string და int ერთად ვერ ჩაიწერება
+a=4
+b=2
+print(a+b)
+
+# discount=0.5 * price
+# print(discount)
+# price=7             თანმიმდევრობა არაა დაცული
+price=7
+discount=0.5 * price
+print(discount)
+
+# print(country)      country-ს არ აქვს value
+country="Georgia"
+print(country)
+
+# name=Irakli         name არის ბრჭყალების გარეშე და ამის გამო error წარმოიქმნა და აღარ გაგრძელდა კოდი
+# age=17
+# city="Poti"
+# print(city)
+name="Irakli"
+age=17
+city="Poti"
+print(city)
+
+# debugging არის შეცდომის პოვნა და მისი გასწორება
---
+++ day 4/homework/homework.py
@@ -0,0 +1,42 @@
+#შექმენით ცვლადი, სახელად points, რომელიც თავიდან უდრის 50, შემდეგ გაანახლეთ ამ ცვლადის მნიშვნელობა ორჯერ, საბოლოოდ კი დაპრინტეთ ეს ცვლადი.
+points=50  
+points=100
+points=150
+print(points)
+
+#დაწერეთ 10 ხაზი კოდი (შეცდომებით) და ქვემოთ მიუწერეთ სწორი ვერსია.
+# score="50"
+# name=Irakli
+# country
+# num1=thirty five
+# print(num1)
+# title=Interstellar
+# print=title
+# print(age)
+# age=17
+# num2-->37
+num1=37
+num2=35
+score=50
+age=17
+country="Georgia"
+name="Irakli"
+title="Interstellar"
+print(title)
+print(country)
+print(age)
+
+#შექმენით ცვლადი სადაც შეინახავთ თქვენს საყვარელ რიცხვს, შექმენით მეორე ცვლადი სადაც შეინახავთ თქვენს ასაკს და შექმენით მესამე ცვლადი სადაც შეინახავთ ამ ცვლადების ნამრავლს.
+a=7
+b=17
+c=a*b
+print(c)
+
+#ცვლადების მეშვეობით დაბეჭდეთ მათემატიკური გამოსახულებელი, ყველა განვილილი მათემატიკური ოპერატორით.
+a=5
+b=8
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
